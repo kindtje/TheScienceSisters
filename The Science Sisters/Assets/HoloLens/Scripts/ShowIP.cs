c2a0dc0d7cfce21587144126958f986a17fbc3e2
Florian <florian@8d-games.nl>
2018-05-09 10:02:12 +0200

    first video feed working, but all in hololens folder

---
+++ The Science Sisters/Assets/HoloLens/Scripts/ShowIP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using UnityEngine.UI;
+
+public class ShowIP : MonoBehaviour
+{
+	void Start () {
+		string IP = Network.player.ipAddress;
+		Text text = GetComponent<Text>();
+		text.text = "IP:\n" + IP;
+	}
+}
